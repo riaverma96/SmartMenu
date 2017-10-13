b8e2c44eb4201e80352e66445542a5b376034fa7
Ria Verma <riaverma@lawn-128-61-7-108.lawn.gatech.edu>
2017-10-13 14:23:03 -0400

    Added Home page, Sign in and Sign up page.

---
+++ SmartMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartMenu
+//
+//  Created by Ria Verma on 9/29/17.
+//  Copyright © 2017 Hex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
