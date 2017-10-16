a541353c80281a51164373b2cae3d8d1b96fa855
Ria Verma <riaverma96@outlook.com>
2017-10-15 21:05:40 -0400

    Update MapViewController.swift

--- SmartMenu/MapViewController.swift
+++ SmartMenu/MapViewController.swift
@@ -23,20 +23,91 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
 
         map.setRegion(region, animated: true)
         self.map.showsUserLocation = true
+        readAllRestaurantData()
+        
+        let pin = RestaurantPin(title: "", subtitle: "", coordinate : userLocation)
+        map.addAnnotation(pin)
     }
-    
+    func readAllRestaurantData() {
+        do {
+            
+            let file = Bundle.main.url(forResource: "all_atlanta_restaurants", withExtension: "json")
+            
+            if (file != nil) {
+                
+                let data = try Data(contentsOf: file!)
+                
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                
+                let object = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                
+                if let dictionary = object as? [String: AnyObject] {
+                    
+                    //if let object = json as? [String: Any] {
+                    
+                    // json is a dictionary
+                    
+                    print("json is a dictionary")
+                    
+                    readJSONObject(object: dictionary)
+                    
+                    // print(object)
+                    
+                } else if json is [Any] {
+                    
+                    // json is an array
+                    
+                    print("json is an array")
+                    
+                    // print(object)
+                    
+                } else {
+                    
+                    print("JSON is invalid")
+                    
+                }
+                
+            } else {
+                
+                print("no file")
+                
+            }
+            
+        } catch {
+            
+            print(error.localizedDescription)
+            
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        
+        
+        
     }
     
+    func readJSONObject(object: [String: AnyObject]) {
+        
+        guard let restaurants = object["restaurants"] as? [[String: AnyObject]] else { return }
+        
+        for restaurant in restaurants {
+            
+            guard let latitude = restaurant["latitude"] as? Double,
+            let name = restaurant["name"] as? String,
+            let longitude = restaurant["longitude"] as? Double else { break }
+            let restaurantLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+            map.addAnnotation(RestaurantPin(title: name, subtitle: "", coordinate : restaurantLocation))
+            
+        }
+        
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
