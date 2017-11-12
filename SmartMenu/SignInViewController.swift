a5ddf36dc3ed905f86190a1b62c6765749ba7723
xiaoyushowyou <xiaoyushowyou@gmail.com>
2017-11-11 22:19:44 -0500

    Add sign in and sign up function

--- SmartMenu/SignInViewController.swift
+++ SmartMenu/SignInViewController.swift
@@ -7,24 +7,39 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class SignInViewController: UIViewController {
     @IBOutlet weak var signInTextField: UITextField!
     
     @IBOutlet weak var passWordTextField: UITextField!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be  recreated.
     }
     
-
+    @IBAction func signInFunction(_ sender: Any) {
+        if let email = signInTextField.text, let pass = passWordTextField.text
+        {
+            Auth.auth().createUser(withEmail: email, password: pass, completion: { (user, error) in
+                if let u = user{
+                    self.performSegue(withIdentifier: "Map", sender: self)
+                }
+                else {
+                    //error
+                }
+            })
+        }
+        
+    }
     /*
     // MARK: - Navigation
 
