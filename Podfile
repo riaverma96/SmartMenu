16b4fcaf6933f38851882a6a1245efd6a7c09c46
xiaoyushowyou <xiaoyushowyou@gmail.com>
2017-11-27 23:23:51 -0500

    add push notification function

--- Podfile
+++ Podfile
@@ -6,4 +6,5 @@ target 'SmartMenu' do
   pod 'Firebase/Core'
   pod 'Firebase/Database'
   pod 'Firebase/Auth'
+  pod 'Firebase/Messaging'
 end
--- SmartMenu/AppDelegate.swift
+++ SmartMenu/AppDelegate.swift
@@ -10,20 +10,48 @@ import Firebase
 import UIKit
 import Stripe
 
+import UserNotifications
+import FirebaseInstanceID
+import FirebaseMessaging
+
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate{
     
     var window: UIWindow?
     
-    internal func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey:Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey:Any]?) -> Bool {
         //        let rootVC = BrowseViewController()
         //        let navController = UINavigationController(rootViewController: rootVC)
         //        let window = UIWindow(frame: UIScreen.main.bounds)
         //        window.rootViewController = navController
         //        window.makeKeyAndVisible()
         //        self.window = window
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        
+        let token = Messaging.messaging().fcmToken
+        print("FCM token: \(token ?? "")")
+        
         FirebaseApp.configure()
         return true
     }
+    
+    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
+        print("Firebase registration token: \(fcmToken)")
+    }
+    
 }
 
